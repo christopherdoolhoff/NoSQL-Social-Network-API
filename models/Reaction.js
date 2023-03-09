f5195e1eec48216bb8fcb3e67e6c59fc183c8452
CDoolhoff <109476450+christopherdoolhoff@users.noreply.github.com>
2023-03-09 10:08:37 -0500

    created reactionSchema in models/Reaction.js

---
+++ models/Reaction.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require("mongoose");
+const moment = require("moment");
+
+// Reaction schema
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtStamp) =>
+        moment(createdAtStamp).format("MMM DD, YYYY [at] hh:mm a"),
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+module.exports = reactionSchema;
